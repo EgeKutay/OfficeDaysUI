ab3d46fbabffb70b8e6bf34458c74d1bc6ba3cc2
Ege Kutay <egekyurusen@gmail.com>
2022-05-29 20:42:29 +0300

    First init

---
+++ src/layouts/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="navbar-container d-flex content align-items-center">
+
+    <!-- Nav Menu Toggler -->
+    <ul class="nav navbar-nav d-xl-none">
+      <li class="nav-item">
+        <b-link
+          class="nav-link"
+          @click="toggleVerticalMenuActive"
+        >
+          <feather-icon
+            icon="MenuIcon"
+            size="21"
+          />
+        </b-link>
+      </li>
+    </ul>
+
+    <!-- Left Col -->
+    <div class="bookmark-wrapper align-items-center flex-grow-1 d-none d-lg-flex">
+      <dark-Toggler class="d-none d-lg-block" />
+    </div>
+
+    <b-navbar-nav class="nav align-items-center ml-auto">
+     <userdropdown />
+    </b-navbar-nav>
+  </div>
+</template>
+
+<script>
+import {
+  BLink, BNavbarNav, BNavItemDropdown, BDropdownItem, BDropdownDivider, BAvatar,
+} from 'bootstrap-vue'
+import DarkToggler from '@core/layouts/components/app-navbar/components/DarkToggler.vue'
+import userdropdown from "./UserDropdown.vue"
+import UserDropdown1 from '@/@core/layouts/components/app-navbar/components/UserDropdown.vue'
+export default {
+  components: {
+    BLink,
+    BNavbarNav,
+    BNavItemDropdown,
+    BDropdownItem,
+    BDropdownDivider,
+    BAvatar,
+    userdropdown,
+
+    // Navbar Components
+    DarkToggler,
+    UserDropdown1,
+  },
+  props: {
+    toggleVerticalMenuActive: {
+      type: Function,
+      default: () => {},
+    },
+  },
+}
+</script>
---
+++ src/store/api-services/auth.module.js
@@ -0,0 +1,42 @@
+import useJwt from "@/auth/jwt/useJwt"
+import util from "@/auth/utils"
+const state={
+  user:[]
+
+
+}
+
+const getters={
+  getUserData(){
+    console.log(user)
+    return user
+  }
+
+}
+const actions={
+async login(context,data){
+  let response
+    context.commit('getLoginData', response=await useJwt.login(data))
+    return response
+  }
+
+  }
+  
+
+const mutations={
+  getLoginData(state,response){
+    state.user=JSON.parse(JSON.stringify(response))
+    useJwt.setToken(state.user.data.token)
+    useJwt.setRefreshToken(state.user.data.refreshToken)
+    localStorage.setItem('userData',JSON.stringify(response.data))
+    console.log(response.data)
+
+  }
+
+}
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
