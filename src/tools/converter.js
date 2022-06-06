b409e0ac64e705cdee5dd6394345e7b214e4e129
Ege Kutay <egekyurusen@gmail.com>
2022-06-06 20:06:35 +0300

    added specification

---
+++ src/tools/converter.js
@@ -0,0 +1,16 @@
+
+"@param json model"
+function Json2ExcelFormat(jsonmodel){
+//excel format means the data model is ready to exported to excel
+//this function will convert default config file to excel
+}
+"@param excel model"
+function Excel2JsonFormat(excelmodel){
+    //this function will convert excel model to default config
+
+}
+export default{
+    Json2ExcelFormat,
+    Excel2JsonFormat
+
+}
